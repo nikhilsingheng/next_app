09568c83ab42df47f9159c17c28073876c6b71dc
nikhil <nikhilsingheng@gmail.com>
2024-02-29 23:10:03 +0530

    new code

---
+++ comman/Layout.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+export default function Layout({ children }) {
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,23 @@
-import "@/styles/globals.css";
+import React, { useEffect, useState } from "react";
+import Layout from "@/comman/Layout";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const [showing, setShowing] = useState(false);
+  useEffect(() => {
+    setShowing(true);
+  }, []);
+  if (!showing) {
+    return null;
+  }
+  if (typeof window === "undefined") {
+    return <></>;
+  } else {
+    return (
+      <>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </>
+    );
+  }
 }
